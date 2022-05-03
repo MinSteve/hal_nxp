7493f104d24ca6d53145bdcdc8c0d11884ceb641
Dung Tran Anh <min98st1@gmail.com>
2022-05-03 12:12:34 +0700

    [S32K_SDK] Modify cmake file for a driver Modify path and name of driver for cmake file
    Signed-off-by: Dung Tran Anh <min98st1@gmail.com>

--- s32k/s32k-sdk/drivers/src/pins/driver_pins.cmake
+++ s32k/s32k-sdk/drivers/src/pins/driver_pins.cmake
@@ -1,9 +1,9 @@
-#Description: ACMP Driver; user_visible: True
+#Description: Pins Driver; user_visible: True
 include_guard(GLOBAL)
-message("driver_edma component is included.")
+message("driver_pins component is included.")
 
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/edma_driver.c
+    ${CMAKE_CURRENT_LIST_DIR}/pins_driver.c
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
